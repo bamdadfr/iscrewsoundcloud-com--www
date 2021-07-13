a2b3e4e4c246c8b5b842b95359c32af90be72837
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-07-13 11:18:11 +0200

    chore(release): add npm step
    [force-release]

--- release.config.js
+++ release.config.js
@@ -5,13 +5,17 @@ module.exports = {
         ['@semantic-release/changelog', {
             'changelogFile': 'CHANGELOG.md',
         }],
+        ['@semantic-release/npm', {
+            'npmPublish': false,
+        }],
         '@semantic-release/github',
         ['@semantic-release/git', {
             'assets': [
                 'CHANGELOG.md',
+                'package.json',
             ],
-            // eslint-disable-next-line no-undef
-            'message': `chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}`,
+            // eslint-disable-next-line no-template-curly-in-string
+            'message': 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
         }],
     ],
 }
